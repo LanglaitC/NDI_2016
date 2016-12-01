1f496182c1a219f434117a600bb976cfb3f16ece
Peslier Lucien <lucien.peslier@etu.unistra.fr>
2016-12-01 22:44:15 +0100

    Push home page

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  var content = $(".main-content");
+  var menu_li = $(".main-menu-list").children();
+  var slided = false;
+  $(".main-content-button_enter").on("click", function(){
+      content.addClass("open");
+      slided = true;i=0;
+      menu_li.each(function(index){
+        var e = $(this);
+        setTimeout(function(){
+          console.log(e);
+          e.addClass("open-li");
+        }, 250*index);
+      });
+  });
+  $(".main-content-button_close").on("click", function(){
+    content.removeClass("open");
+    menu_li.removeClass("open-li");
+  });
+});
